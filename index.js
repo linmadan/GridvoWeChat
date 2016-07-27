8fbbf296a8b6adc19208fd85a8d9b7dcd4b46f57
linmadan <772181827@qq.com>
2016-07-27 14:32:07 +0800

    增加2个新的微信API应用服务

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ var constant = require('./lib/application/util/constant');
 var bearcatContextPath = require.resolve('./bcontext.json');
 bearcat.createApp([bearcatContextPath]);
 var createCallBackURLAuthService;
+var createParseCallBackDataService;
 var createSuiteTicketManageService;
 var createSuiteAccessTokenManageService;
 var createAuthCorpManageService;
+var createCorpUserManageService;
 var createAuthCorpSuiteProxyService;
 var createCorpAuthSuiteService;
 bearcat.start(function () {
     createCallBackURLAuthService = function () {
         return bearcat.getBean('callBackURLAuthService');
+    };
+    createParseCallBackDataService = function () {
+        return bearcat.getBean('parseCallBackDataService');
     };
     createSuiteTicketManageService = function () {
         return bearcat.getBean('suiteTicketManageService');
@@ -28,11 +33,16 @@ bearcat.start(function () {
     createCorpAuthSuiteService = function () {
         return bearcat.getBean('corpAuthSuiteService');
     };
+    createCorpUserManageService = function () {
+        return bearcat.getBean('corpUserManageService');
+    };
 });
 module.exports.createCallBackURLAuthService = createCallBackURLAuthService;
+module.exports.createParseCallBackDataService = createParseCallBackDataService;
 module.exports.createSuiteTicketManageService = createSuiteTicketManageService;
 module.exports.createSuiteAccessTokenManageService = createSuiteAccessTokenManageService;
 module.exports.createAuthCorpManageService = createAuthCorpManageService;
 module.exports.createAuthCorpSuiteProxyService = createAuthCorpSuiteProxyService;
 module.exports.createCorpAuthSuiteService = createCorpAuthSuiteService;
+module.exports.createCorpUserManageService = createCorpUserManageService;
 module.exports.constant = constant;
